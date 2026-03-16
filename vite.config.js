4b977f0f82e197475238479e87c9c6f4f51ed11b
Andrey <noo_@bk.ru>
2026-03-16 22:52:12 +0300

    +8

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,17 @@
 import { defineConfig } from 'vite'
 import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  root: __dirname,
+  root: process.cwd(),
   server: {
     port: 3000,
     open: true,
   },
   build: {
-    outDir: path.resolve(__dirname, 'dist'),
+    outDir: path.resolve(process.cwd(), 'dist'),
     emptyOutDir: true,
     rollupOptions: {
-      input: path.resolve(__dirname, 'index.html'),
+      input: path.resolve(process.cwd(), 'index.html'),
     },
   },
 })
